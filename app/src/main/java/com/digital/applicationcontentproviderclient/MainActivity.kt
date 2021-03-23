f4be0306b86e8fb850943ece981e33bac2b3259b
Edson Shideki Kokado <eskokado@gmail.com>
2021-03-23 06:13:01 -0300

    Finishing with the getContentProvider method

--- app/src/main/java/com/digital/applicationcontentproviderclient/MainActivity.kt
+++ app/src/main/java/com/digital/applicationcontentproviderclient/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.digital.applicationcontentproviderclient
 
+import android.database.Cursor
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,9 +20,25 @@ class MainActivity : AppCompatActivity() {
 
         noteRefreshButton = findViewById(R.id.client_buttom_refresh)
         notesRecycler = findViewById(R.id.client_list)
+        getContentProvider()
 
         noteRefreshButton.setOnClickListener {
+            getContentProvider()
+        }
+    }
 
+    private fun getContentProvider() {
+        try {
+            val url = "content://com.digital.applicationcontentprovider.provider/notes"
+            val data: Uri = Uri.parse(url)
+            val cursor: Cursor? =
+                contentResolver.query(data, null, null, null, "title")
+            notesRecycler.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = ClientAdapter(cursor as Cursor)
+            }
+        } catch (ex: Exception) {
+            ex.printStackTrace()
         }
     }
 }
